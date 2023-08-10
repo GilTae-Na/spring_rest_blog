12c64e15619657e21f2ec20784ad6525e5efec06
nagiltae <nagt1997@gmail.com>
2023-08-10 19:00:50 +0900

    feat : get article method

--- src/main/java/com/example/springdeveloper/BlogApiController.java
+++ src/main/java/com/example/springdeveloper/BlogApiController.java
@@ -1,13 +1,17 @@
 package com.example.springdeveloper;
 
 import com.example.springdeveloper.domain.Article;
+import com.example.springdeveloper.dto.ArticleResponse;
 import com.example.springdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +23,13 @@ public class BlogApiController {
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
         Article saveArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(saveArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll().stream().map(ArticleResponse::new).toList();
+
+        return ResponseEntity.ok().body(articles);
     }
 
 }
--- src/main/java/com/example/springdeveloper/dto/ArticleResponse.java
+++ src/main/java/com/example/springdeveloper/dto/ArticleResponse.java
@@ -1,4 +1,17 @@
 package com.example.springdeveloper.dto;
 
+import com.example.springdeveloper.domain.Article;
+import lombok.Getter;
+
+@Getter
 public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article){
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+
 }
