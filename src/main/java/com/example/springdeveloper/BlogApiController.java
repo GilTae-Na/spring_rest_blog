24cf7a6008d0a91c05eb76e0be548028b4056192
nagiltae <nagt1997@gmail.com>
2023-08-10 20:48:31 +0900

    feat : add findArticleById and testCode

--- src/main/java/com/example/springdeveloper/BlogApiController.java
+++ src/main/java/com/example/springdeveloper/BlogApiController.java
@@ -6,10 +6,7 @@ import com.example.springdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +28,14 @@ public class BlogApiController {
 
         return ResponseEntity.ok().body(articles);
     }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id){
+
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok().body(new ArticleResponse(article));
+    }
+
 
 }
